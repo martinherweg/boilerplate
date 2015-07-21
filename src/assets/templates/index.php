fdee53b7cb2dea8cc57c3c5272e5b16f768c7472
Martin Herweg <m.herweg@conceptbakery.de>
2015-07-21 14:10:41 +0200

    moved all assets to src folder

---
+++ src/assets/templates/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>test</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+  Dies ist ein test
+  <script src="  //cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js
+"></script>
+<script src="assets/js/plugins.js"></script>
+<script src="assets/js/scripts.min.js"></script>
+</body>
+</html>
